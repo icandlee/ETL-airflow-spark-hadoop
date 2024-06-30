e7a6abc7b7c3401d775f2854b4566861d9b7e4c5
hyunsoo park <hyunsoo2936@naver.com>
2024-06-30 21:23:43 +0900

    feat:버스 승하차 인원 조회 api 클래스 구현

---
+++ utils/api/bus_passenger_api.py
@@ -0,0 +1,23 @@
+from ..common.public_data_api import SeoulPublicDataAPI
+
+class BusPassengerData(SeoulPublicDataAPI):
+    """
+    버스 승하차 인원 데이터를 가져오기 위한 클래스.
+    """
+    def __init__(self):
+
+        super().__init__()
+    
+    def get_passenger_data(self, bus_stop_id, date):
+        """
+        특정 버스 정류장과 날짜의 승하차 인원 데이터를 URL 경로에 파라미터를 붙여 가져오는 메서드.
+        
+        Args:
+            bus_stop_id (str): 버스 정류장 ID.
+            date (str): 데이터를 조회할 날짜 (형식: YYYYMMDD).
+        
+        Returns:
+            dict: 버스 승하차 인원 데이터.
+        """
+        endpoint = "/BUS_API_ENDPOINT"  
+        return self.fetch_data(endpoint, bus_stop_id, date)
